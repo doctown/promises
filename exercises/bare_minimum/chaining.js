4da300a9af412b11659d52868bfef2a3391cd1c4
David Ogor <doctown@yahoo.com>
2016-03-23 13:04:25 -0700

    Progress with Sprint

--- exercises/bare_minimum/chaining.js
+++ exercises/bare_minimum/chaining.js
@@ -63,30 +63,30 @@ var addNewUserToDatabaseAsync = function(user) {
   return db.findUserInDatabaseAsync(user)
     .then(function(existingUser) {
       if (existingUser) {
-        throw new Error('User already exists!') // Head straight to `catch`. Do not pass Go, do not collect $200
+        throw new Error('User already exists!'); // Head straight to `catch`. Do not pass Go, do not collect $200
       } else {
         return user; // Return a syncronous value
       }
     })
     .then(function(newUser) {
-      return db.hashPasswordAsync(newUser) // Return a promise
+      return db.hashPasswordAsync(newUser); // Return a promise
     })
     .then(function(securedUser) {
-      return db.createAndSaveUserAsync(securedUser) // Return another promise
+      return db.createAndSaveUserAsync(securedUser); // Return another promise
     })
 }
 
 // Uncomment the lines below and run the example with `node exercises/bare_minimum/chaining.js`
 // It will succeed most of the time, but fail occasionally to demonstrate error handling
 
-// addNewUserToDatabaseAsync({ name: 'Dan', password: 'chickennuggets' })
-//   .then(function(savedUser) {
-//     console.log('All done!')
-//   })
-//   .catch(function(err) {
-//     // Will catch any promise rejections or thrown errors in the chain!
-//     console.log('Oops, caught an error: ', err.message)
-//   });
+ addNewUserToDatabaseAsync({ name: 'Dan', password: 'chickennuggets' })
+   .then(function(savedUser) {
+     console.log('All done!')
+   })
+   .catch(function(err) {
+     // Will catch any promise rejections or thrown errors in the chain!
+     console.log('Oops, caught an error: ', err.message)
+   });
 
 /******************************************************************
  *                         Exercises                              *
@@ -100,12 +100,23 @@ var addNewUserToDatabaseAsync = function(user) {
 
 // HINT: We already wrote some similar promise returning functions
 var pluckFirstLineFromFileAsync = require('./promiseConstructor').pluckFirstLineFromFileAsync;
-var getGitHubProfileAsync = require('./promisification').getGitHubProfileAsync
+var getGitHubProfileAsync = require('./promisification').getGitHubProfileAsync;
 
 
 
 var fetchProfileAndWriteToFile = function(readFilePath, writeFilePath) {
-  // TODO
+  return pluckFirstLineFromFileAsync(readFilePath)
+    .then(function (userName) {
+      return getGitHubProfileAsync(userName);
+    }).then(function (response) {
+      new Promise(function(resolve, reject) {
+        fs.writeFile(writeFilePath, response, function(err) {
+          reject(err);
+        });
+      });
+    }).catch(function(err) {
+      console.log(err);
+    });
 };
 
 module.exports = {
